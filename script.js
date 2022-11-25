40da6b40e02419fc388760f3a955446ef0929bca
WAM <eraserw@gmail.com>
2022-11-25 11:08:30 +0100

    feat: fct addticket

--- script.js
+++ script.js
@@ -1,68 +1,63 @@
 const INPUT = document.getElementById('input');
-const USERid = document.getElementById('userID');
+const USERid = document.getElementById('select');
 
 
 fetch(`https://webhelprequest.deta.dev/users`) // test get simple !!
     .then((response) => response.json())
     .then((data) => {
         console.log(data);
-        let datA = data.data ;
+        let datA = data.data;
         affichUser(datA);
 
-
-
     })
-    .catch((error) => { console.log(error) })
+    .catch((error) => { console.log(error) });
 
-let input = INPUT.value ;
-    function addTicket(input, idUser) {
 
-        const options = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: new URLSearchParams({ subject: 'help', userId: 'username' })
-        };
-    
-        fetch("url-ticket", options)
-            .then(response => response.json())
-            .then(ticket => console.log(ticket))
-            .catch(err => console.error(err));
+
+let input = INPUT.value;
+let idUser = USERid.value;
+let idTicket =0;
+
+function addTicket(input, idUser) {
+
+    idTicket++;
+
+    const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: new URLSearchParams({ subject: `${input}`, userId: `${idUser}` })
     };
-    
-/* {
-    fetch(`https://webhelprequest.deta.dev/tickets`) // test get simple !!--ticket
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data.data.length);
 
+    fetch("url-ticket", options)
+        .then(response => response.json())
+        .then(ticket => console.log(ticket))
+        .catch(err => console.error(err));
 
+        addRowTab(cptTicket,idUser,input)
+};
 
+function addRowTab (idTicket,idUser,input){
 
-        })
-        .catch((error) => { console.log(error) })
+                let affichage = `<tr class="" >
+                    <td scope="row">${idTicket}</td>
+                    <td>${idUser}</td>
+                    <td>${input}</td>
+                    <td>"fct°supprimer ticket/row"</td>`;
+
 };
 
 
 
- */
+
 
 
 function affichUser(dataUser) {
-        console.log(dataUser[1].username);
-        let affichage = `<select class="form-select" id="floatingSelect" aria-label="Floating label select example">`;
-            affichage +=     `<option selected>Selectionnez votre prénom :</option>`;
-        for (let i = 0; i < dataUser.length; i++) {
-            affichage += `<option value="" >${dataUser[i].username}</option>`;
-            console.log(dataUser[i].username);
-        };
-        document.getElementById('select').innerHTML = affichage;
-
-    /*     let affichage += select 
-    affichage += option selected
-    for (i){
-        affichage += option nom
-    }
-    affichage += /select 
-    odcument.queryselector nouveau div . innerrHTMl = affichage
-    */
+    console.log(dataUser[1].username);
+    let affichage = `<select class="form-select" id="floatingSelect" aria-label="Floating label select example">`;
+    affichage += `<option selected>Selectionnez votre prénom :</option>`;
+    for (let i = 0; i < dataUser.length; i++) {
+        affichage += `<option id="userID" value="" >${dataUser[i].username}</option>`;
+        console.log(dataUser[i].username);
+    };
+    document.getElementById('select').innerHTML = affichage;
 };
