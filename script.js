c4c341c0202ae09d34df13d9882fe0f12a1c67ba
WAM <eraserw@gmail.com>
2022-11-27 23:28:26 +0100

    pull23h30-271122

--- script.js
+++ script.js
@@ -2,13 +2,11 @@ const INPUT = document.getElementById('input');
 const USERid = document.getElementById('select');
 const HELP = document.getElementById('help');
 const NEXT = document.getElementById('next');
-const BIN = document.getElementById('bin');
-
 
 
 let idTicket = 0;
 let idUser = "";
-let bin = `<button type="button" class="btn btn-danger mx-auto" id="bin"><svg xmlns="http://www.w3.org/2000/svg" width="16"
+let bin = `<button type="submit" formtarget="_self" class="btn btn-danger mx-auto"><svg xmlns="http://www.w3.org/2000/svg" width="16"
 height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
 <path
     d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
@@ -50,21 +48,12 @@ HELP.addEventListener('click', event => {
 
 //mise en place du bouton "Suivant" qui supprime le premier ticket de la liste/tableau qui sera considérer donc traiter !!
 NEXT.addEventListener('click', event => {
-    //event.preventDefault();
 
     let ligne = document.getElementById('table').firstChild;
 
 
     ligne.parentNode.removeChild(ligne);
     ticketOff(ligne.firstChild.textContent);
-})
-
-BIN.addEventListener('click', event => {
-    let ligneBin = document.getElementById('table').currentTarget;
-    ligneBin.parentNode.removeChild(ligneBin);
-    ticketOff(ligneBin.currentTarget.textContent);
-
-
 })
 
 
@@ -119,25 +108,10 @@ function addRowTab(idTicket, idUser, input) {
     newRow.appendChild(addCase(idTicket));
     newRow.appendChild(addCase(idUser));
     newRow.appendChild(addCase(input));
-    newRow.appendChild(addBin());
+    newRow.appendChild(addBin(idTicket));
     affichage.appendChild(newRow);
 }
 
-/* function bin () {
-    let affichage = document.getElementById('table');
-    let poubelle = `<svg xmlns="http://www.w3.org/2000/svg" width="16"
-height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-<path
-    d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-<path fill-rule="evenodd"
-    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
-</svg>`;
-    const newRow = document.createElement(`button type="button" class="btn btn-danger mx-auto`);
-    const newBin = document.innerHTML(poubelle);
-    newRow.appendChild(newBin);
-    return affichage.appendChild(newRow);
-
-}; */
 
 //fonction qui crée une case pour les lignes du tableau
 function addCase(data) {
@@ -149,10 +123,11 @@ function addCase(data) {
 }
 
 //fct de creation de la case du bouton poubelle
-function addBin() {
+function addBin(idTicket) {
     let affichage = document.getElementById('table');
     const newRowBin = document.createElement("td");
     newRowBin.innerHTML = bin;
+    newRowBin.setAttribute('id', `${idTicket}`);
     return affichage.appendChild(newRowBin);
 }
 
