44c3eb1d465e10d8b54bdcefb13ea86dfc4b6c92
WAM <eraserw@gmail.com>
2022-11-27 13:08:28 +0100

    feat: ajout commentaires

--- script.js
+++ script.js
@@ -5,111 +5,108 @@ const NEXT = document.getElementById('next');
 
 
 
-let idTicket =0;
+let idTicket = 0;
 let idUser = "";
 
-
+//requete API des tickets toujours en attente à l'ouverture de la session
 fetch("https://webhelprequest.deta.dev/tickets")
-        .then(response => response.json())
-        .then(ticket => previous(ticket))
-        .catch(err => console.error(err));
-        
-
-function previous (data){
-        const DATA = data.data;
-        let nbTicket = DATA.length;
-        for(let i =0; i<nbTicket;i++){
-            addRowTab(DATA[i].key,DATA[i].users_id,DATA[i].subject)
-        }
+    .then(response => response.json())
+    .then(ticket => previous(ticket))
+    .catch(err => console.error(err));
 
 
+    //remplissage du tableau à l'ouverture de la session
+function previous(data) {
+    const DATA = data.data;
+    let nbTicket = DATA.length;
+    for (let i = 0; i < nbTicket; i++) {
+        addRowTab(DATA[i].key, DATA[i].users_id, DATA[i].subject)
+    }
+}
 
-USERid.addEventListener('change', function (event){
-    event.preventDefault();
-    idUser = document.getElementById(`userID${this.value}`).textContent;
-    return INPUT.value = "";
-})
+//mise en place de la selection du prénom
+    USERid.addEventListener('change', function (event) {
+        event.preventDefault();
+        idUser = document.getElementById(`userID${this.value}`).textContent;
+        return INPUT.value = "";
+    })
 
-HELP.addEventListener('click', event =>{
-    event.preventDefault();
-    let input = INPUT.value;
-    return addTicket(input,idUser);
-})
+    //mise en place du bouton "demande d'aide"
+    HELP.addEventListener('click', event => {
+        event.preventDefault();
+        let input = INPUT.value;
+        return addTicket(input, idUser);
+    })
 
-NEXT.addEventListener('click', event =>{
-    //event.preventDefault();
+    //mise en place du bouton "Suivant" qui supprime le premier ticket de la liste/tableau qui sera considérer donc traiter !!
+    NEXT.addEventListener('click', event => {
+        //event.preventDefault();
 
-    let ligne = document.getElementById('table').firstChild;
-    
-    ligne.parentNode.removeChild(ligne);
-    
-})
+        let ligne = document.getElementById('table').firstChild;
 
-
-fetch(`https://webhelprequest.deta.dev/users`) // test get simple !!
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-        let datA = data.data;
-        affichUser(datA);
+        ligne.parentNode.removeChild(ligne);
 
     })
-    .catch((error) => { console.log(error) });
+
+//requete API pour connaitre les prénoms des personnes qui peuvent utiliser les tickets !
+    fetch(`https://webhelprequest.deta.dev/users`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            let datA = data.data;
+            affichUser(datA);
+
+        })
+        .catch((error) => { console.log(error) });
 
 
-function affichUser(dataUser) {
-    console.log(dataUser[1].username);
-    let affichage = `<select class="form-select" id="floatingSelect" aria-label="Floating label select example">`;
-    affichage += `<option selected>Selectionnez votre prénom :</option>`;
-    for (let i = 0; i < dataUser.length; i++) {
-        affichage += `<option id="userID${i}" value="${i}" >${dataUser[i].username}</option>`;
-        console.log(dataUser[i].username);
+//fonction qui affiche les prénoms
+    function affichUser(dataUser) {
+        console.log(dataUser[1].username);
+        let affichage = `<select class="form-select" id="floatingSelect" aria-label="Floating label select example">`;
+        affichage += `<option selected>Selectionnez votre prénom :</option>`;
+        for (let i = 0; i < dataUser.length; i++) {
+            affichage += `<option id="userID${i}" value="${i}" >${dataUser[i].username}</option>`;
+            console.log(dataUser[i].username);
+        };
+        document.getElementById('select').innerHTML = affichage;
     };
-    document.getElementById('select').innerHTML = affichage;
-};
 
+//fonction qui permet d'ajouter un ticket à l'API et au tableau
+    function addTicket(input, idUser) {
 
-function addTicket(input, idUser) {
+        idTicket++;
 
-    idTicket++;
+        const options = {
+            "method": 'POST',
+            "headers": { 'Content-Type': 'application/x-www-form-urlencoded' },
+            "body": new URLSearchParams({ subject: `${input}`, userId: `${idUser}` })
+        };
 
-    const options = {
-        "method": 'POST',
-        "headers": { 'Content-Type': 'application/x-www-form-urlencoded' },
-        "body": new URLSearchParams({ subject: `${input}`, userId: `${idUser}` })
+        fetch("https://webhelprequest.deta.dev/tickets", options)
+            .then(response => response.json())
+            .then(ticket => console.log(ticket))
+            .catch(err => console.error(err));
+
+        addRowTab(idTicket, idUser, input)
     };
 
-    fetch("https://webhelprequest.deta.dev/tickets", options)
-        .then(response => response.json())
-        .then(ticket => console.log(ticket))
-        .catch(err => console.error(err));
+    //fonction qui ajoute une ligne au tableau
+    function addRowTab(idTicket, idUser, input) {
 
-        addRowTab(idTicket,idUser,input)
-};
+        let affichage = document.getElementById('table');
+        const newRow = document.createElement("tr");
+        newRow.appendChild(addCase(idTicket));
+        newRow.appendChild(addCase(idUser));
+        newRow.appendChild(addCase(input));
+        affichage.appendChild(newRow);
+    }
 
-function addRowTab (idTicket,idUser,input){
-
-                let affichage = document.getElementById('table');
-                const newRow = document.createElement("tr");
-                newRow.appendChild(addCase(idTicket));
-                newRow.appendChild(addCase(idUser));
-                newRow.appendChild(addCase(input));
-                affichage.appendChild(newRow);
-}
-
-function addCase (data) {
-    let affichage = document.getElementById('table');
-    const newRow = document.createElement("td");
-    const content = document.createTextNode(data)
-    newRow.appendChild(content);
-    return affichage.appendChild(newRow);
-}
-
-
-
-
-
-
-
-            
-}
+    //fonction qui crée une case pour les lignes du tableau
+    function addCase(data) {
+        let affichage = document.getElementById('table');
+        const newRow = document.createElement("td");
+        const content = document.createTextNode(data)
+        newRow.appendChild(content);
+        return affichage.appendChild(newRow);
+    }
