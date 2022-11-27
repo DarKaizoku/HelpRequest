1cf3aabc9bae922e14d29c5cfcc27d6e0ebc5778
Titou <menardjc.33@gmail.com>
2022-11-27 16:01:31 +0100

    travail jc sur le boutton bin

--- script.js
+++ script.js
@@ -16,6 +16,13 @@ const NEXT = document.getElementById('next');
 
 let idTicket = 0;
 let idUser = "";
+/* let bin = `<button type="button" class="btn btn-danger mx-auto"><svg xmlns="http://www.w3.org/2000/svg" width="16"
+height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+<path
+    d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+<path fill-rule="evenodd"
+    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+</svg></button>` */
 
 //requete API des tickets toujours en attente à l'ouverture de la session
 fetch("https://webhelprequest.deta.dev/tickets")
@@ -102,13 +109,14 @@ function addTicket(input, idUser) {
 };
 
 //fonction qui ajoute une ligne au tableau
-function addRowTab(idTicket, idUser, input) {
+function addRowTab(idTicket, idUser, input, bin) {
 
     let affichage = document.getElementById('table');
     const newRow = document.createElement("tr");
     newRow.appendChild(addCase(idTicket));
     newRow.appendChild(addCase(idUser));
     newRow.appendChild(addCase(input));
+    /* newRow.appendChild(addCase(bin)); */
     affichage.appendChild(newRow);
 }
 
@@ -120,6 +128,13 @@ function addCase(data) {
     newRow.appendChild(content);
     return affichage.appendChild(newRow);
 }
+/* function addBin () {
+    let affichage = document.getElementById('table');
+    const newRowBin = document.createElement("td");
+    const binContent = document.innerHTML('bin');
+    newRowBin.appendChild(binContent);
+    return affichage.appendChild(newRowBin);
+} */
 
 
 //fonction qui desactive les tickets en attente
