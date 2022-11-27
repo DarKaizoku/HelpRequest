be4ea65a2839541c0e154c1134fdd11ac2c9697a
Titou <menardjc.33@gmail.com>
2022-11-27 20:06:47 +0100

    commit jc avant pull

--- script.js
+++ script.js
@@ -16,13 +16,7 @@ const NEXT = document.getElementById('next');
 
 let idTicket = 0;
 let idUser = "";
-/* let bin = `<button type="button" class="btn btn-danger mx-auto"><svg xmlns="http://www.w3.org/2000/svg" width="16"
-height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-<path
-    d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-<path fill-rule="evenodd"
-    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
-</svg></button>` */
+
 
 //requete API des tickets toujours en attente à l'ouverture de la session
 fetch("https://webhelprequest.deta.dev/tickets")
@@ -118,9 +112,25 @@ function addRowTab(idTicket, idUser, input, bin) {
     newRow.appendChild(addCase(idTicket));
     newRow.appendChild(addCase(idUser));
     newRow.appendChild(addCase(input));
-    /* newRow.appendChild(addCase(bin)); */
+    newRow.appendChild(addBin(bin));
     affichage.appendChild(newRow);
 }
+
+/* function bin () {
+    let affichage = document.getElementById('table');
+    let poubelle = `<svg xmlns="http://www.w3.org/2000/svg" width="16"
+height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+<path
+    d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+<path fill-rule="evenodd"
+    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+</svg>`;
+    const newRow = document.createElement(`button type="button" class="btn btn-danger mx-auto`);
+    const newBin = document.innerHTML(poubelle);
+    newRow.appendChild(newBin);
+    return affichage.appendChild(newRow);
+
+}; */
 
 //fonction qui crée une case pour les lignes du tableau
 function addCase(data) {
@@ -130,13 +140,23 @@ function addCase(data) {
     newRow.appendChild(content);
     return affichage.appendChild(newRow);
 }
-/* function addBin () {
+
+let bin = `<button type="button" class="btn btn-danger mx-auto><svg xmlns="http://www.w3.org/2000/svg" width="16"
+height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+<path
+    d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+<path fill-rule="evenodd"
+    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+</svg></button>SSS`;
+
+function addBin(bin){
     let affichage = document.getElementById('table');
-    const newRowBin = document.createElement("td");
-    const binContent = document.innerHTML('bin');
-    newRowBin.appendChild(binContent);
-    return affichage.appendChild(newRowBin);
-} */
+    const newRow = document.createElement("td");
+    const newBin = document.createTextNode(bin)
+    newRow.appendChild(newBin);
+    return affichage.appendChild(newRow);
+}
+    
 
 
 //fonction qui desactive les tickets en attente
